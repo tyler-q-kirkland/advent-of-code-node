b074dd57677a00792aa1f38ad479fea668176225
Tyler Kirkland <tyler.q.kirkland@gmail.com>
2024-12-02 08:38:13 -0800

    Removing vars

--- 2024/day2/part1.js
+++ 2024/day2/part1.js
@@ -1,31 +1,33 @@
 import utils from '../../utils/index.js';
 
-let year = 2024;
-let day = 2;
-let part = 'A';
+const year = 2024;
+const day = 2;
+const part = 'A';
 let sum = 0;
 
 const input = utils.getInput();
 
 for (let line of input) {
-    var levelArray = line.split(" ");
-    var validLevel = true;
+    const levelArray = line.split(" ");
+    let validLevel = true;
     validLevel = validateLevel(levelArray);
     sum += validLevel ? 1 : 0;
 
 }
 
 function validateLevel(levelArray) {
-    var validLevel = true;
+    let validLevel = true;
+    let initialDirection;
     for (let i = 0; i < levelArray.length - 1 && validLevel == true; i++) {
+        let currentDirection;
         if (i == 0) {
-            var initialDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
+            initialDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
         } else {
-            var currentDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
+            currentDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
         }
         let diff = Math.abs(levelArray[(i)] - levelArray[i + 1])
         if (diff > 3 || diff < 1 || (i !== 0 && currentDirection !== initialDirection)) {
-            var validLevel = false;
+            validLevel = false;
         }
     }
     return validLevel;
--- 2024/day2/part2.js
+++ 2024/day2/part2.js
@@ -1,36 +1,39 @@
 import utils from '../../utils/index.js';
 
-let year = 2024;
-let day = 2;
-let part = 'B';
+const year = 2024;
+const day = 2;
+const part = 'B';
 let sum = 0;
 
 const input = utils.getInput();
 
 for (let line of input) {
-    var levelArray = line.split(" ");
-    var validLevel = validateLevel(levelArray);
+    const levelArray = line.split(" ");
+    let validLevel = true;
+    validLevel = validateLevel(levelArray);
     sum += validLevel ? 1 : 0;
 }
 
 function validateLevel(levelArray, backupFlag) {
-    var validLevel = true;
+    let validLevel = true;
+    let initialDirection;
     for (let i = 0; i < levelArray.length - 1 && validLevel == true; i++) {
+        let currentDirection;
         if (i == 0) {
-            var initialDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
+            initialDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
         } else {
-            var currentDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
+            currentDirection = (+levelArray[i] < +levelArray[i + 1]) ? 'up' : 'down';
         }
         let diff = Math.abs(levelArray[(i)] - levelArray[i + 1])
         if (diff > 3 || diff < 1 || (i !== 0 && currentDirection !== initialDirection)) {
             if (backupFlag !== true) {
-                var backupValidity = false;
+                let backupValidity = false;
                 for (let n = 0; n < levelArray.length && backupValidity !== true; n++) {
                     let backupArray = [...levelArray];
                     backupArray.splice(+n, 1);
-                    var backupValidity = validateLevel(backupArray, true);
+                    backupValidity = validateLevel(backupArray, true);
                 }
-                var validLevel = backupValidity;
+                validLevel = backupValidity;
             } else { validLevel = false }
         }
     }
